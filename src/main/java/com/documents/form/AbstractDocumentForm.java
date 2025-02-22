dca87d379cde82bd08482a77a04e01a64b4b475b
Tfactor2020@gmail.com <Tfactor08>
2025-02-22 13:49:11 +0300

    add primary form for adding new documents

---
+++ src/main/java/com/documents/form/AbstractDocumentForm.java
@@ -0,0 +1,55 @@
+package com.documents.form;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import com.documents.model.AbstractDocument;
+
+public abstract class AbstractDocumentForm extends Stage {
+
+    protected AbstractDocument document;
+    protected Button submitButton;
+    protected Map<String, TextField> field_textField;
+
+    public AbstractDocumentForm(AbstractDocument document, String[] fields) {
+        this.setTitle(document.getName());
+        this.initModality(Modality.APPLICATION_MODAL);
+
+        field_textField = new HashMap<String, TextField>();
+
+        for (String field : fields)
+            field_textField.put(field, new TextField());
+
+        // Layout
+        VBox vbox = new VBox();
+
+        // Create UI components
+        submitButton = new Button("Ok");
+        submitButton.setOnAction(event -> {
+            close();
+        });
+
+        for (var field_textField_items : field_textField.entrySet()) {
+            var label = new Label(field_textField_items.getKey());
+            var textField = field_textField_items.getValue();
+            vbox.getChildren().addAll(label, textField);
+        }
+        vbox.getChildren().add(this.submitButton);
+
+        // Set the scene
+        Scene scene = new Scene(vbox);
+        this.setScene(scene);
+    }
+
+    //protected abstract void initializeForm();
+    public abstract AbstractDocument getFilledDocument();
+}
