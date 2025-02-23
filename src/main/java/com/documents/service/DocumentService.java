6ee3bcad14ae7e055b41a889362d69a35c293099
Tfactor2020@gmail.com <Tfactor08>
2025-02-23 23:06:57 +0300

    add service layer

---
+++ src/main/java/com/documents/service/DocumentService.java
@@ -0,0 +1,13 @@
+package com.documents.serice;
+
+import com.documents.model.AbstractDocument;
+
+public class DocumentService {
+    public void saveToFile(String path) {
+
+    }
+
+    public AbstractDocument readFromFile(String path) {
+        return null;
+    }
+}
